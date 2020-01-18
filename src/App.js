7f42b6a58f6516b53376a984630a150e845fcad9
Pedro Costa <pcon1996@gmail.com>
2020-01-17 23:04:03 -0300

    Desenvolvimento da interface e consumindo a API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import api from './services/api'
+
+import './global.css';
+import './app.css';
+import './sidebar.css';
+import './main.css';
+
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+
+
+// Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+// Propriedade: Informação que um componente PAI para para os componentes FILHOS
+// Estado: Informações mantidas pelo componentes (imutabilidade)
 
 function App() {
+  const [devs, setDevs] = useState([]);
+  
+
+  useEffect(()=>{
+    async function loadDevs(){
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data){
+    const response = await api.post('/devs', data);
+    setDevs([...devs, response.data]);
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+
+      </main>
     </div>
   );
 }
